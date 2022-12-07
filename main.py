43fed4016c137089eafe363f7c4cfabe401c403f
Simon Peier <simon.w.peier@gmail.com>
2022-12-07 12:55:06 +0100

    Add year flag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import datetime
 import pathlib
 import random
 import shutil
@@ -9,7 +10,7 @@ import requests
 
 
 def fetch_file(year):
-    url = 'https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen_od1002/download/KUL100OD1002.csv'
+    url = "https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen_od1002/download/KUL100OD1002.csv"
     response = requests.get(url)
     response.encoding = "utf-8-sig"
 
@@ -27,14 +28,20 @@ def parse_arguments():
     subparsers = parser.add_subparsers()
 
     parser_find = subparsers.add_parser("find")
-    parser_find.add_argument('name', type=str)
+    parser_find.add_argument("name", type=str)
+    parser_find.add_argument("-y", "--year", type=int, default=datetime.datetime.now().year,
+                             help="year to get data from")
     parser_find.set_defaults(func=find)
 
     parser_stats = subparsers.add_parser("stats")
+    parser_stats.add_argument("-y", "--year", type=int, default=datetime.datetime.now().year,
+                              help="year to get data from")
     parser_stats.set_defaults(func=stats)
 
     parser_create = subparsers.add_parser("create")
-    parser_create.add_argument('-o', '--output-dir', type=str, default=None)
+    parser_create.add_argument("-o", "--output-dir", type=str, default=None)
+    parser_create.add_argument("-y", "--year", type=int, default=datetime.datetime.now().year,
+                               help="year to get data from")
     parser_create.set_defaults(func=create_new_dog)
 
     args = parser.parse_args()
@@ -44,7 +51,7 @@ def parse_arguments():
 def download_dog_media_file(name, year, path):
     # Get link of random dog media file
     res = requests.get("https://random.dog/woof.json").json()
-    media_url = res['url']
+    media_url = res["url"]
 
     # Download said dog media file
     file_extension = pathlib.Path(media_url).suffix
@@ -52,7 +59,7 @@ def download_dog_media_file(name, year, path):
     out_path = (pathlib.Path(path) if path else pathlib.Path.cwd()) / filename
     res = requests.get(media_url, stream=True)
     if res.status_code == 200:
-        with open(out_path, 'wb') as file:
+        with open(out_path, "wb") as file:
             res.raw.decode_content = True
             shutil.copyfileobj(res.raw, file)
 
@@ -60,9 +67,11 @@ def download_dog_media_file(name, year, path):
 
 
 def create_new_dog(args):
+    dog_list = fetch_file(args.year)
+
     name = random.choice([dog[1] for dog in dog_list])
     year = random.choice([dog[2] for dog in dog_list])
-    sex = random.choice(['m', 'f'])
+    sex = random.choice(["m", "f"])
     path = args.output_dir
     media_filename = download_dog_media_file(name, year, path)
 
@@ -74,11 +83,15 @@ def create_new_dog(args):
 
 
 def find(args):
+    dog_list = fetch_file(args.year)
+
     matching_dogs = ([dog[1], dog[2], dog[4][0]] for dog in dog_list if dog[1] == args.name)
     print(list(matching_dogs))
 
 
 def stats(args):
+    dog_list = fetch_file(args.year)
+
     overall = [dog[1] for dog in dog_list if "?" not in dog[1]]
     male = [dog[1] for dog in dog_list if dog[3] == 1]
     female = [dog[1] for dog in dog_list if dog[3] == 2]
@@ -93,6 +106,5 @@ def stats(args):
     print(f"Number of female dogs: {len(female)}")
 
 
-if __name__ == '__main__':
-    dog_list = fetch_file(2015)
+if __name__ == "__main__":
     parse_arguments()
